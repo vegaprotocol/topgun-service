77b1aff5326ebaac5b081c9564ddf3df94203b39
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2023-02-22 11:17:45 +0000

    Median algos

---
+++ leaderboard/median.go
@@ -0,0 +1,24 @@
+package leaderboard
+
+import (
+	"sort"
+)
+
+func median(data []float64) float64 {
+	dataCopy := make([]float64, len(data))
+	copy(dataCopy, data)
+
+	sort.Float64s(dataCopy)
+
+	var median float64
+	l := len(dataCopy)
+	if l == 0 {
+		return 0
+	} else if l%2 == 0 {
+		median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
+	} else {
+		median = dataCopy[l/2]
+	}
+
+	return median
+}
